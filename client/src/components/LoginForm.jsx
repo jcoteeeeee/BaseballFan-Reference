7601d9ebf7d97e21c9e94b94ea8ce8bc3398e68e
John Cote <johncote61598@gmail.com>
2023-01-30 13:24:21 -0500

    created loginform component

---
+++ client/src/components/LoginForm.jsx
@@ -0,0 +1,20 @@
+const LoginForm = () => {
+    return(
+        <div>
+            <form>
+                <label>Username</label>
+                <input id='username' name='username' required />
+                <br/>
+                <label>Email</label>
+                <input id='email' name='email' required />
+                <br/>
+                <label>Password</label>
+                <input id='password' name='password' required />
+                <br/>
+                <button>Login</button>
+            </form>
+        </div>
+    )
+}
+
+export default LoginForm 
