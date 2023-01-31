6046d48e1c749d58d9802f6aa62783c9484d32df
John Cote <johncote61598@gmail.com>
2023-01-31 14:51:45 -0500

    created game table and give it table headers

--- client/src/components/ProfilePage.jsx
+++ client/src/components/ProfilePage.jsx
@@ -4,6 +4,20 @@ const ProfilePage = () => {
     return(
         <div>
             <Header/> 
+            <div id='game-table'>
+                <table>
+                    <tr>
+                        <th>Date</th>
+                        <th>Result</th>
+                        <th>Score</th>
+                        <th>Opponnent</th>
+                        <th>Location</th>
+                        <th>Starting Pitcher</th>
+                        <th>Cumulative Record</th>
+                        <th>Note</th>
+                    </tr>
+                </table>       
+            </div>
         </div>
     )
 }
