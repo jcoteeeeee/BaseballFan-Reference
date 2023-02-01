79a9922ba62fb1befda54eb2a3cc4bf5b4b796f5
John Cote <johncote61598@gmail.com>
2023-02-01 10:33:20 -0500

    created controllers and added routes for user and game

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,5 +2,17 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
-end
+  # root "articles#index" 
+
+  get 'users', to: 'users#index'
+  get 'users/:id', to: 'users#show'
+  post 'users', to: 'users#create'
+  patch 'users/:id', to: 'users#update'
+  delete 'users/:id', to: 'users#destroy'
+
+  get 'games', to: 'games#index'
+  get 'games/:id', to: 'games#show'
+  post 'games', to: 'games#create'
+  patch 'games/:id', to: 'games#update'
+  delete 'games/:id', to: 'games#destroy'
+end 
