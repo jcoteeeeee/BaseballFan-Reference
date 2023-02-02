59b5898869af6c9bbf55ed61e4e58830b27544c3
John Cote <johncote61598@gmail.com>
2023-02-02 11:30:43 -0500

    added routing to go from profilepage to addgamepage and editgamepage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import Homepage from './components/Homepage'
 import ProfilePage from './components/ProfilePage'
 import Login from './components/Login'
-import Signup from'./components/Signup'
+import Signup from'./components/Signup'   
+import AddGamePage from './components/AddGamePage'  
+import EditGamePage from './components/EditGamePage'
 import {Routes, Route, useNavigate} from 'react-router-dom'
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'  
 
 function App() {
   const navigate = useNavigate() 
@@ -29,7 +31,16 @@ function App() {
   // // takes you from signup page to profile page 
   // const handleSignup = () => {
   //   navigate('/profilepage')
-  // }
+  // } 
+
+  // takes you from profile page to add game page 
+  const handleAddGameBtn = () => {
+    navigate('/addgame')
+  } 
+
+  const handleEditGameBtn = () => {
+    navigate('/editgame')
+  }
 
   //get request for all users
   useEffect(() => {
@@ -75,7 +86,9 @@ function App() {
         <Route exact path='/' element={<Homepage handleLogBtnClick={handleLogBtnClick} handleSignupClick={handleSignupClick}/>} /> 
         <Route exact path='login' element={<Login handleLogin={handleLogin}/>} />
         <Route exact path='signup' element={<Signup handleSignupSubmit={handleSignupSubmit}/>} />
-        <Route exact path='/profilepage' element={<ProfilePage games={games}/>} />
+        <Route exact path='/profilepage' element={<ProfilePage games={games} handleAddGameBtn={handleAddGameBtn} handleEditGameBtn={handleEditGameBtn}/>} />
+        <Route exact path='/addgame' element={<AddGamePage />} /> 
+        <Route exact path='/editgame' element={<EditGamePage/>} />
       </Routes>
     </div>
   )
