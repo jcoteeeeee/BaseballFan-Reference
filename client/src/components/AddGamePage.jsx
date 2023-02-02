1e6392a05ec2ea93e8dcefbf7afba8e68c840e51
John Cote <johncote61598@gmail.com>
2023-02-02 10:41:34 -0500

    added form to both addgamepage and editgamepage

--- client/src/components/AddGamePage.jsx
+++ client/src/components/AddGamePage.jsx
@@ -1,8 +1,27 @@
+import Header from './Header.jsx'
+
 const AddGamePage = () => {
     return(
         <div>
-
-        </div>
+            <form id='addgame-form'>
+                <label>Date</label>
+                <input type='date' name='date' />
+                <br />
+                <label>Result</label>
+                <input type='text' name='result' />
+                <br />
+                <label>Score</label>
+                <input type='text' name='score' />
+                <br />
+                <label>Location</label>
+                <input type='text' name='location' />
+                <br />
+                <label>Note</label>
+                <input type='text' name='note' />
+                <br />
+                <button type='submit'>Add game</button>
+            </form>    
+        </div>  
     )
 }  
 
