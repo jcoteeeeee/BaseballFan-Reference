248fab43c3e69d050485ecb7fba0e6293bbd6999
John Cote <johncote61598@gmail.com>
2023-02-02 15:14:54 -0500

    made action to create user

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -9,5 +9,9 @@ class UsersController < ApplicationController
         render json: user
     end 
 
+    def create 
+        user = User.create(first_name: params[:first_name], last_name: params[:last_name], username: params[:username], email: params[:email], password: params[:password], fav_team: params[:fav_team], photo: params[:photo])
+        render json: user
+    end 
     
 end
