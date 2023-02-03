bf79409f2ca54d52f7350754885f6c82fb12ee24
John Cote <johncote61598@gmail.com>
2023-02-03 10:19:42 -0500

    added post request for addgamepage. Doesn't work

--- client/src/components/AddGamePage.jsx
+++ client/src/components/AddGamePage.jsx
@@ -6,19 +6,21 @@ const AddGamePage = () => {
             <Header/>
             <form id='addgame-form'>
                 <label>Date</label>
-                <input type='date' name='date' />
+                <input type='date' name='add_date' />
                 <br />
                 <label>Result</label>
-                <input type='text' name='result' />
+                <input type='text' name='add_result' />
                 <br />
                 <label>Score</label>
-                <input type='text' name='score' />
+                <input type='text' name='add_score' />
                 <br />
                 <label>Location</label>
-                <input type='text' name='location' />
+                <input type='text' name='add_location' />
                 <br />
+                <label>Starting pitcher </label>  
+                <input type='text' name='add_stp'/>
                 <label>Note</label>
-                <input type='text' name='note' />
+                <input type='text' name='add_note' />
                 <br />
                 <button type='submit'>Add game</button>
             </form>    
