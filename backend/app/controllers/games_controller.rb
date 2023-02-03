bfa935f4e013a897c52e92e47db67b86e1bb012d
John Cote <johncote61598@gmail.com>
2023-02-03 15:42:21 -0500

    fixed issue on backend regarding addgame

--- backend/app/controllers/games_controller.rb
+++ backend/app/controllers/games_controller.rb
@@ -10,13 +10,13 @@ class GamesController < ApplicationController
     end 
 
     def create 
-        game = Game.create(date: params[:date], result: params[:result], score: params[:score], opponent: params[:opponent], location: params[:location], st_pitcher: params[:st_pithcer], note: params[:note]) 
+        game = Game.create(user_id: params[:user_id], date: params[:date], result: params[:result], score: params[:score], opponent: params[:opponent], location: params[:location], st_pitcher: params[:st_pitcher], note: params[:note]) 
         render json: game 
     end 
 
     def update 
         game = Game.find_by(id: params[:id])
-        game.update(date: params[:date], result: params[:result], score: params[:score], opponent: params[:opponent], location: params[:location], st_pitcher: params[:st_pithcer], note: params[:note])  
+        game.update(date: params[:date], result: params[:result], score: params[:score], opponent: params[:opponent], location: params[:location], st_pitcher: params[:st_pitcher], note: params[:note])  
         render json: game 
     end 
 
