ccebc55d23e1b7c9befd5402ca676a2cb20feeca
John Cote <johncote61598@gmail.com>
2023-02-06 11:47:22 -0500

    added opponent to addgame and editgame form and got editgame component further set up

--- client/src/components/AddGamePage.jsx
+++ client/src/components/AddGamePage.jsx
@@ -6,7 +6,8 @@ const AddGamePage = () => {
     const navigate = useNavigate()
     const [date, setDate] = useState('')
     const [result, setResult] = useState('')
-    const [score, setScore] = useState('')
+    const [score, setScore] = useState('')  
+    const [opponent, setOpponent] = useState('')
     const [location, setLocation] = useState('')
     const [stPitcher, setStPitcher] = useState('') 
     const [note, setNote] = useState('')
@@ -32,7 +33,8 @@ const AddGamePage = () => {
                     user_id: 1, // needs to be changed later to variable. 1 is placeholder for now so post request works 
                     date: date, 
                     result: result,
-                    score: score,
+                    score: score,  
+                    opponent: opponent, 
                     location: location,
                     st_pitcher: stPitcher,
                     note: note
@@ -47,22 +49,25 @@ const AddGamePage = () => {
             <Header/>
             <form id='addgame-form' onSubmit={handleAddGameSubmit}>
                 <label>Date</label>
-                <input value={date} id='add-date' type='date' name='date' onChange={(e) => setDate(e.target.value)}/> {/* make required */}
+                <input value={date} id='add-date' type='date' name='add_date' onChange={(e) => setDate(e.target.value)}/> {/* make required */}
                 <br />
                 <label>Result</label>
-                <input value={result} id='add-result' type='text' name='result' onChange={(e) => setResult(e.target.value)}/>
+                <input value={result} id='add-result' type='text' name='add_result' onChange={(e) => setResult(e.target.value)}/>
                 <br />
                 <label>Score</label>
-                <input value={score} id='add-score' type='text' name='score' onChange={(e) => setScore(e.target.value)}/>
-                <br />
+                <input value={score} id='add-score' type='text' name='add_score' onChange={(e) => setScore(e.target.value)}/>
+                <br />   
+                <label>Opponent</label>
+                <input value={opponent} id='add-opponent' type='text' name='add_opp' onChange={(e) => setOpponent(e.target.value)}/>
+                <br/>
                 <label>Location</label>
-                <input value={location} id='add-location' type='text' name='location' onChange={(e) => setLocation(e.target.value)}/>
+                <input value={location} id='add-location' type='text' name='add_location' onChange={(e) => setLocation(e.target.value)}/>
                 <br />
                 <label>Starting pitcher </label>  
-                <input value={stPitcher} id='add-stp' type='text' name='stPitcher' onChange={(e) => setStPitcher(e.target.value)}/>  
+                <input value={stPitcher} id='add-stp' type='text' name='add_stp' onChange={(e) => setStPitcher(e.target.value)}/>  
                 <br/>
                 <label>Note</label>
-                <input value={note} id='add-note' type='text' name='note' onChange={(e) => setNote(e.target.value)}/>
+                <input value={note} id='add-note' type='text' name='add_note' onChange={(e) => setNote(e.target.value)}/>
                 <br />
                 <button type='submit'>Add game</button>
             </form>    
--- client/src/components/EditGamePage.jsx
+++ client/src/components/EditGamePage.jsx
@@ -1,26 +1,62 @@
 import Header from './Header.jsx'  
+import {useState} from 'react'
+import {useNavigate} from 'react-router-dom'
 
-const EditGamePage = ( {handleEditGameSubmit} ) => {
+const EditGamePage = () => {
+    const navigate = useNavigate()  
+    const [date, setDate] = useState('')
+    const [result, setResult] = useState('')
+    const [score, setScore] = useState('')  
+    const [opponent, setOpponent] = useState('')
+    const [location, setLocation] = useState('')
+    const [stPitcher, setStPitcher] = useState('')  
+    const [note, setNote] = useState('')  
+
+    console.log(date)
+
+    const handleEditGameSubmit = (e) => {
+        e.preventDefault()  
+        console.log('submitted') 
+        navigate('/profilepage')   
+        const editGame = async (e, games) => {
+            let req = await fetch(`http://localhost:3000/games/${games.id}`, {
+                method: 'PATCH', 
+                headers: {'Content-Type': 'application/json'}, 
+                body: JSON.stringify({
+                    user_id: 1, 
+                    date: date, 
+                    score: score, 
+                    opponent: opponent, 
+                    location: location, 
+                    st_pitcher: stPitcher, 
+                    note: note
+                })
+            })
+        }
+    } 
+
     return(
         <div>
             <Header/>  
             <form id='editgame-form' onSubmit={() => {handleEditGameSubmit}}>
                 <label>Date</label>
-                <input type='date' name='edit_date' />
+                <input value={date} type='date' name='edit_date' onChange={(e) => setDate(e.target.value)}/>
                 <br />
                 <label>Result</label>
-                <input type='text' name='edit_result' />
+                <input value={result} type='text' name='edit_result' onChange={(e) => setResult(e.target.value)}/>
                 <br />
                 <label>Score</label>
-                <input type='text' name='edit_score' />
-                <br />
+                <input value={score} type='text' name='edit_score' onChange={(e) => setScore(e.target.value)}/>
+                <br />  
+                <label>Opponent</label>
+                <input value={opponent} type='text' name='edit_opp' onChange={(e) => setOpponent(e.target.value)}/>
                 <label>Location</label>
-                <input type='text' name='edit_location' />
+                <input value={location} type='text' name='edit_location' onChange={(e) => setLocation(e.target.value)}/>
                 <label>Starting pitcher</label>
-                <input type='text' name='edit_stp'/>
+                <input value={stPitcher} type='text' name='edit_stp' onChange={(e) => setStPitcher(e.target.value)}/>
                 <br />
                 <label>Note</label>
-                <input type='text' name='edit_note' />
+                <input value={note} type='text' name='edit_note' onChange={(e) => setNote(e.target.value)}/>
                 <br />
                 <button type='submit'>Save changes</button>
             </form>    
