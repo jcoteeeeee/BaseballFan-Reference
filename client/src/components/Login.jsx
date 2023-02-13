c8f4668aa28b70bf169c9ca545e2076d19a0049d
John Cote <johncote61598@gmail.com>
2023-02-13 16:36:15 -0500

    styling login page

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -4,13 +4,14 @@ const Login = ( {handleLogin} ) => {
     return(
         <div>
             <Header/> 
-            <form id='login-form' onSubmit={handleLogin}>
+            <form id='login-form' onSubmit={handleLogin}> 
+                <h2>Login</h2>
                 <label>Email</label>
-                <input id='email' name='email' /> {/*make required later */}
+                <input id='email' className='login-input' name='email' /> {/*make required later */}
                 <br/>
                 <label>Password</label>
-                <input id='password' name='password' /> {/*make required later */}
-                <br/>
+                <input id='password' className='login-input' name='password' /> {/*make required later */}
+                <br/> 
                 <button type='submit'>Login</button>
             </form>
         </div>
