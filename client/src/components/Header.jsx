eb489763b6b9158d896e2e2dd536b80d8499728b
John Cote <johncote61598@gmail.com>
2023-02-14 15:37:01 -0500

    routing on click for about works. Trying to get follow to work

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,16 +1,22 @@
-import {useNavigate} from 'react-router-dom' 
+import {useNavigate, Link} from 'react-router-dom' 
 
 const Header = () => {
     const navigate = useNavigate()           
+    
     const handleLogoClick = () => {
         navigate('/')
     }
+
+    const handleAboutClick = () => {
+        navigate('/about')
+    }  
+
     return(
         <div id='header'>
             <img id='logo' onClick={handleLogoClick} src='./photos/bfr-logo.png'/>  
             <hr /> 
-            <h2 id='to-about'>ABOUT</h2>
-            <h2 id='to-follow'>FOLLOW</h2> 
+            <h2 id='to-about' onClick={handleAboutClick}>ABOUT</h2>
+            <h2 id='to-follow'>FOLLOW</h2>     
         </div>
     )
 }
