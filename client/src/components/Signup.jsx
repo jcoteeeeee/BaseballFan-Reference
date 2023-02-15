2f06237d2c895c2f201009341288cb9d451616a1
John Cote <johncote61598@gmail.com>
2023-02-15 14:34:41 -0500

    signup and login seem to be working. So does logout

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -8,12 +8,11 @@ const Signup = () => {
         email: '',
         password: '',
     })
+    console.log(loginData)
 
     const navigate = useNavigate()
 
-    console.log(loginData)
-
-    const handleSignup = (e) => {
+    const handleLogin = (e) => {
         const { name, value } = e.target
         setLoginData((prev) => {
             return { ...prev, [name]: value };
@@ -22,10 +21,41 @@ const Signup = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault()
-        const { data } = await http.post("/signup", loginData)
+        const { data } = await http.post("/signup", loginData) 
         alert('Account created!') 
         navigate('/')
-    }
+    } 
+    
+    // const navigate = useNavigate()
+    // const [firstName, setFirstName] = useState('')
+    // const [lastName, setLastName] = useState('')
+    // const [favTeam, setFavTeam] = useState('') 
+    // const [username, setUsername] = useState('')
+    // const [email, setEmail] = useState('')
+    // const [password, setPassword] = useState('')  
+
+    // console.log(username)
+
+    // const handleSignupSubmit = (e) => {
+    //     e.preventDefault()
+    //     console.log('submitted')
+    //     navigate('/profilepage') // right now it's not going to profilepage
+    //     const signup = async (e) => {
+    //         let req = await fetch('http://localhost:3000/users', {
+    //             method: 'POST',
+    //             headers: { 'Content-Type': 'application/json' },
+    //             body: JSON.stringify({
+    //                 first_name: firstName,
+    //                 last_name: lastName,   
+    //                 fav_team: favTeam, 
+    //                 username: username,
+    //                 email: email,
+    //                 password: password
+    //             })
+    //         })
+    //     }
+    //     signup()
+    // } 
 
     return(
         <div>
@@ -34,10 +64,10 @@ const Signup = () => {
                 <form id='signup-form' onSubmit={handleSubmit}> 
                     <h2>Sign up</h2>
                     <label className='login-label'>Email</label>
-                    <input id='email' className='login-input' name='email' align='left' onChange={handleSignup} /> {/*make required later */}
+                    <input id='email' className='login-input' name='email' align='left' onChange={handleLogin} /> {/*make required later */}
                     <br />
                     <label className='login-label'>Password</label>
-                    <input id='password' className='login-input' name='password' align='left' onChange={handleSignup} /> {/*make required later */}
+                    <input id='password' className='login-input' name='password' align='left' onChange={handleLogin} /> {/*make required later */}
                     <br />
                     <button id='signup-btn' type='submit'>Create account </button>
                 </form>  
